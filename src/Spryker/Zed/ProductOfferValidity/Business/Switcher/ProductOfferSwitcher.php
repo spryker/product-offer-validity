5c65741bed66e703a4e6992fb1b68f282884f8c6
Ilya Kubanov <45764294+ilyakubanov@users.noreply.github.com>
2020-07-01 19:12:46 +0300

    BO-577: Create an Offer
    * BO-462: Create the offer form.
    * BO-529: adjusted ProductOffer facade with plugins.
    * BO-532: added Validity to the form.
    * BO-541: added stores to the form.
    * BO-530: added prices to the form.
    * BO-444: functional tests (Create offer).
    * BO-902: Introduced plugins and facades for Product offer extending.
    * BO-1056: fixed navigation is not highlighted on the Add offer page.
    * BO-1401: Symfony web components form theme: added extra property "required" to <form-item> tag, removed it from input attribute.
    
    Co-authored-by: Dmytro Abramov <dmytro.abramov@spryker.com>
    Co-authored-by: Aleksey Belan <alexey.belan@spryker.com>
    Co-authored-by: Stanislav Matveyev <stanislav.matveyev@spryker.com>
    Co-authored-by: Fedets Maxim <maxim.fedets@spryker.com>
    Co-authored-by: Theodoros Liokos <theodoros.liokos@spryker.com>

--- src/Spryker/Zed/ProductOfferValidity/Business/Switcher/ProductOfferSwitcher.php
+++ src/Spryker/Zed/ProductOfferValidity/Business/Switcher/ProductOfferSwitcher.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Spryker Marketplace License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Zed\ProductOfferValidity\Business\ProductOffer;
+namespace Spryker\Zed\ProductOfferValidity\Business\Switcher;
 
 use Generated\Shared\Transfer\ProductOfferTransfer;
 use Spryker\Zed\Kernel\Persistence\EntityManager\TransactionTrait;
--- src/Spryker/Zed/ProductOfferValidity/Business/Switcher/ProductOfferSwitcherInterface.php
+++ src/Spryker/Zed/ProductOfferValidity/Business/Switcher/ProductOfferSwitcherInterface.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Spryker Marketplace License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Zed\ProductOfferValidity\Business\ProductOffer;
+namespace Spryker\Zed\ProductOfferValidity\Business\Switcher;
 
 interface ProductOfferSwitcherInterface
 {
